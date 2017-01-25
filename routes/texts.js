c913292ee1a8ec294e18b18a48386c960f2c80f1
Tim <timothee.astier@essec.edu>
2017-01-25 11:08:26 +0100

    Create notFoundChars in DB when saving chars

--- routes/texts.js
+++ routes/texts.js
@@ -1,10 +1,12 @@
 import express from 'express';
+import isEmpty from 'lodash/isEmpty';
 
 import { text as Text } from '../models';
 import { char as Char } from '../models';
 import { charText as CharText } from '../models';
 
 import TextService from '../services/TextService';
+import CharTextService from '../services/CharTextService';
 
 let router = express.Router();
 
@@ -60,31 +62,57 @@ router.put('/:id', (req, res) => {
 
 router.put('/:id/chars', (req, res) => {
   const { newChars, charsToDelete } = req.body;
+  let charTextsToAdd = [];
+  // Find in DB all newChars for this text:
   Char
     .findAll({
       where: { chinese: { in: newChars.map(x => x.chinese) } }
-      // TODO: Add chars if not found
-    }).then((chars) => {
-      const charTextsToAdd = chars.map((char) => {
+    })
+    .then((chars) => {
+      charTextsToAdd = chars.map((char) => {
         return {
           charId: char.id,
           textId: req.params.id
         };
       });
-      // BulkCreate new charTexts
+      // Create notFoundChars in chars DB (if any):
+      let notFoundChars = newChars.filter((x) => {
+        return (chars.map(char => char.chinese).indexOf(x.chinese) === -1);
+      });
+      if (isEmpty(notFoundChars)) {
+        return;
+      } else {
+        notFoundChars = notFoundChars.map(x => {
+          return { chinese: x.chinese };
+        });
+        return Char
+            .bulkCreate(notFoundChars, {returning: true})
+            .then((createdChars) => {
+            // Add charTexts to charTextsToAdd with IDs of newly created chars:
+              return charTextsToAdd = charTextsToAdd.concat(
+                createdChars.map(x => {
+                  return { charId: x.id, textId: req.params.id };
+                })
+              );
+            });
+      }
+    })
+    .then(() => {
+      // Create charTexts in DB:
       return CharText.bulkCreate(charTextsToAdd);
-    }).then(() => {
-      // BulkDestroy charTexts
-      CharText
-      .destroy({
-        where: {
-          id: { in: charsToDelete.map(x => x.charText.id) }
-        }
-      })
-      .then(() => {
-        return TextService.getChars(req.params.id).then(chars => {
-          return res.status(200).json({ chars });
-        });;
+    })
+    .then(() => {
+      // Destroy charTexts in DB:
+      if (isEmpty(charsToDelete)) {
+        return;
+      } else {
+        return CharTextService.destroyCharsToDelete(charsToDelete);
+      }
+    })
+    .then(() => {
+      // Retrieve newly updated list of chars for this text:
+      return TextService.getChars(req.params.id).then(chars => {
+        return res.status(200).json({ chars });
       });
     });
 });
