da030176dc5083aa70ee8117044ee50d73a48ac6
Tim <timothee.astier@essec.edu>
2017-01-30 17:03:28 +0100

    CORS only allow client requests

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ let app = express();
 
 app.use(bodyParser.json());
 
+// Forest Admin setup
 app.use(require('forest-express-sequelize').init({
   modelsDir: __dirname + '/models',
   envSecret: process.env.FOREST_ENV_SECRET,
@@ -21,7 +22,14 @@ app.use(require('forest-express-sequelize').init({
   sequelize
 }));
 
-// TODO: Protect the API to authenticated users only
+// Enable CORS to allow requests from the client
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", process.env.CLIENT_URL);
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+});
+
 app.use('/api/users', users);
 app.use('/api/auth', auth);
 app.use('/api/texts', texts);
