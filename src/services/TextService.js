fb7e45d3941536f102d5dd5547e8cfdaf967c1ce
Tim <timothee.astier@essec.edu>
2017-02-07 16:15:28 +0100

    Avoids sending Texts of manuallyDeleted items

--- src/services/TextService.js
+++ src/services/TextService.js
@@ -7,8 +7,14 @@ export default {
       .then(text => {
         return text.getChars({
           attributes: ['id', 'chinese'],
+          // An example of Sequelize's Eager loading
           include: [{
             model: Text,
+            // We don't take texts on which the item was manuallyDeleted
+            // This allow to correctly calculate origin status
+            // This is an example of filter via join table
+            // See infos on stackoverflow: http://bit.ly/2jYCzd9
+            through: { where: { manuallyDeleted: false } },
             where: { order: { $lt: text.order } },
             attributes: ['title', 'order'],
             order: [
@@ -25,8 +31,12 @@ export default {
       .then(text => {
         return text.getWords({
           attributes: ['id', 'chinese'],
+          // An example of Sequelize's Eager loading
           include: [{
             model: Text,
+            // We don't take texts on which the item was manuallyDeleted
+            // This allow to correctly calculate origin status
+            through: { where: { manuallyDeleted: false } },
             where: { order: { $lt: text.order } },
             attributes: ['title', 'order'],
             order: [
