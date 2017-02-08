dcb7c5370601412894dd91bc6aa071bfdc9d7668
Tim <timothee.astier@essec.edu>
2017-02-08 15:33:11 +0100

    Modify services to work with deserialized data sent by client

--- src/services/CharTextService.js
+++ src/services/CharTextService.js
@@ -2,11 +2,12 @@ import { charText as CharText } from '../models';
 
 export default {
   destroyCharsToDelete: (charsToDelete) => {
+    console.log(charsToDelete)
     return CharText
       .destroy({
         // We do not destroy items that were manually altered
         where: {
-          id: { in: charsToDelete.map(x => x.charText.id) },
+          id: { in: charsToDelete.map(x => x.charTextId) },
           manuallyAdded: false,
           manuallyDeleted: false
         }
