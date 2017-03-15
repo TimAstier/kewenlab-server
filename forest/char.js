7f822f14455589921c64e2cb16ec928ecae7bfb9
Tim <timothee.astier@essec.edu>
2017-03-15 11:32:49 +0100

    Smart segments for used and unused chars/words

---
+++ forest/char.js
@@ -0,0 +1,29 @@
+var Liana = require('forest-express-sequelize');
+var models = require('../src/models');
+const _ = require('lodash');
+
+Liana.collection('char', {
+  segments: [{
+    name: 'used',
+    where: () => {
+      const query = 'SELECT * FROM chars WHERE EXISTS (SELECT * FROM "charTexts" WHERE "charTexts"."charId" = chars.id);';
+
+      return models.sequelize
+        .query(query)
+        .then((chars) => {
+          return { id: { $in: _.map(chars[0], 'id') }};
+        });
+    }
+  }, {
+    name: 'not used',
+    where: () => {
+      const query = 'SELECT * FROM chars WHERE NOT EXISTS (SELECT * FROM "charTexts" WHERE "charTexts"."charId" = chars.id);';
+
+      return models.sequelize
+        .query(query)
+        .then((chars) => {
+          return { id: { $in: _.map(chars[0], 'id') }};
+        });
+    }
+  }]
+});
---
+++ forest/word.js
@@ -0,0 +1,29 @@
+var Liana = require('forest-express-sequelize');
+var models = require('../src/models');
+const _ = require('lodash');
+
+Liana.collection('word', {
+  segments: [{
+    name: 'used',
+    where: () => {
+      const query = 'SELECT * FROM words WHERE EXISTS (SELECT * FROM "wordTexts" WHERE "wordTexts"."wordId" = words.id);';
+
+      return models.sequelize
+        .query(query)
+        .then((words) => {
+          return { id: { $in: _.map(words[0], 'id') }};
+        });
+    }
+  }, {
+    name: 'not used',
+    where: () => {
+      const query = 'SELECT * FROM words WHERE NOT EXISTS (SELECT * FROM "wordTexts" WHERE "wordTexts"."wordId" = words.id);';
+
+      return models.sequelize
+        .query(query)
+        .then((words) => {
+          return { id: { $in: _.map(words[0], 'id') }};
+        });
+    }
+  }]
+});
