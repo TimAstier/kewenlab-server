TimAstier/kewenlab-server

commit 5d6eb06ee6874d6a1e30e733d629ce99681ba17f

Refactore routes

src/models/charText.js
@@ -1,32 +1,12 @@
 export default (sequelize, DataTypes) => {
-  let models = sequelize.models;
-  let CharText = sequelize.define('charText', {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true
-    },
-    charId: {
-      type: DataTypes.INTEGER
-    },
-    textId: {
-      type: DataTypes.INTEGER
-    },
-    order: {
-      type: DataTypes.INTEGER
-    },
-    manuallyAdded: {
-      type: DataTypes.BOOLEAN,
-      allowNull: false,
-      defaultValue: true
-    },
-    manuallyDeleted: {
-      type: DataTypes.BOOLEAN,
-      allowNull: false,
-      defaultValue: false
-    }
+  const models = sequelize.models;
+  const CharText = sequelize.define('charText', {
+    charId: { type: DataTypes.INTEGER },
+    textId: { type: DataTypes.INTEGER },
+    order: { type: DataTypes.INTEGER },
+    manuallyAdded: { type: DataTypes.BOOLEAN },
+    manuallyDeleted: { type: DataTypes.BOOLEAN }
   }, {
-    timestamps: true,
     classMethods: {
       associate: () => {
         CharText.belongsTo(models.char);

src/models/wordText.js
@@ -1,32 +1,12 @@
 export default (sequelize, DataTypes) => {
-  let models = sequelize.models;
-  let WordText = sequelize.define('wordText', {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true
-    },
-    order: {
-      type: DataTypes.INTEGER
-    },
-    wordId: {
-      type: DataTypes.INTEGER
-    },
-    textId: {
-      type: DataTypes.INTEGER
-    },
-    manuallyAdded: {
-      type: DataTypes.BOOLEAN,
-      allowNull: false,
-      defaultValue: true
-    },
-    manuallyDeleted: {
-      type: DataTypes.BOOLEAN,
-      allowNull: false,
-      defaultValue: false
-    }
+  const models = sequelize.models;
+  const WordText = sequelize.define('wordText', {
+    order: { type: DataTypes.INTEGER },
+    wordId: { type: DataTypes.INTEGER },
+    textId: { type: DataTypes.INTEGER },
+    manuallyAdded: { type: DataTypes.BOOLEAN },
+    manuallyDeleted: { type: DataTypes.BOOLEAN }
   }, {
-    timestamps: true,
     classMethods: {
       associate: () => {
         WordText.belongsTo(models.word);

src/models/char.js
@@ -1,23 +1,10 @@
 export default (sequelize, DataTypes) => {
-  let models = sequelize.models;
+  const models = sequelize.models;
 
-  let Char = sequelize.define('char', {
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true
-    },
-    chinese: {
-      type: DataTypes.STRING(1),
-      allowNull: false,
-      unique: true
-    },
-    frequency: {
-      type: DataTypes.INTEGER,
-      unique: true
-    }
+  const Char = sequelize.define('char', {
+    chinese: { type: DataTypes.STRING(1) },
+    frequency: { type: DataTypes.INTEGER }
   }, {
-    timestamps: true,
     classMethods: {
       associate: () => {
         Char.belongsToMany(models.text, { through: 'charText' });

src/models/charWord.js
@@ -1,19 +1,9 @@
 export default (sequelize, DataTypes) => {
-  let models = sequelize.models;
-  let CharWord = sequelize.define('charWord', {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true
-    },
-    charId: {
-      type: DataTypes.INTEGER
-    },
-    wordId: {
-      type: DataTypes.INTEGER
-    }
+  const models = sequelize.models;
+  const CharWord = sequelize.define('charWord', {
+    charId: { type: DataTypes.INTEGER },
+    wordId: { type: DataTypes.INTEGER }
   }, {
-    timestamps: true,
     classMethods: {
       associate: () => {
         CharWord.belongsTo(models.char);

src/models/index.js
@@ -4,34 +4,36 @@ import Sequelize from 'sequelize';
 
 const databaseOptions = {
   logging: false,
-  //logging: console.log,
+  // logging: console.log,
   pool: { maxConnections: 10, minConnections: 1 }
 };
 if (process.env.SSL_DATABASE) {
   databaseOptions.dialectOptions = { ssl: true };
 }
 
-let sequelize = new Sequelize(process.env.DATABASE_URL, databaseOptions);
-let db = {};
+const sequelize = new Sequelize(process.env.DATABASE_URL, databaseOptions);
+const db = {};
 fs
   .readdirSync(__dirname)
-  .filter(function (file) {
+  .filter(file => {
     return (file.indexOf('.') !== 0) && (file !== 'index.js');
   })
-  .forEach(function (file) {
+  .forEach(file => {
+    let model = null;
     try {
-      var model = sequelize['import'](path.join(__dirname, file));
+      model = sequelize.import(path.join(__dirname, file));
       db[model.name] = model;
     } catch (error) {
       // This produces errors in build for unknown reason:
-      //console.error('Model creation error: ' + error);
+      // console.error('Model creation error: ' + error);
     }
   });
-Object.keys(db).forEach(function(modelName) {
+Object.keys(db).forEach(modelName => {
   if ('associate' in db[modelName]) {
     db[modelName].associate(db);
   }
 });
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
+
 module.exports = db;

src/models/text.js
@@ -1,26 +1,11 @@
 export default (sequelize, DataTypes) => {
-  let models = sequelize.models;
+  const models = sequelize.models;
 
-  let Text = sequelize.define('text', {
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true
-    },
-    title: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    content: {
-      type: DataTypes.TEXT,
-      default: ''
-    },
-    order: {
-      type: DataTypes.INTEGER,
-      allowNull: false
-    }
+  const Text = sequelize.define('text', {
+    title: { type: DataTypes.STRING },
+    content: { type: DataTypes.TEXT, defaultValue: '' },
+    order: { type: DataTypes.INTEGER }
   }, {
-    timestamps: true,
     classMethods: {
       associate: () => {
         Text.belongsToMany(models.word, { through: 'wordText' });

src/models/user.js
@@ -1,35 +1,11 @@
 export default (sequelize, DataTypes) => {
   const User = sequelize.define('user', {
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true
-    },
-    username: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true
-    },
-    password_digest: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    active: {
-      type: DataTypes.BOOLEAN,
-      allowNull: false,
-      default: false
-    },
-    hidden_words: {
-      type: DataTypes.ARRAY(DataTypes.INTEGER),
-      default: []
-    }
+    username: { type: DataTypes.STRING },
+    email: { type: DataTypes.STRING },
+    password_digest: { type: DataTypes.STRING },
+    active: { type: DataTypes.BOOLEAN, defaultValue: false },
+    hidden_words: { type: DataTypes.ARRAY(DataTypes.INTEGER), defaultValue: [] }
   }, {
-    timestamps: true,
     classMethods: {
       associate: () => {
       }

src/models/word.js
@@ -2,27 +2,10 @@ export default (sequelize, DataTypes) => {
   const models = sequelize.models;
 
   const Word = sequelize.define('word', {
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true
-    },
-    chinese: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true
-    },
-    frequency: {
-      type: DataTypes.INTEGER,
-      unique: true
-    },
-    banned: {
-      type: DataTypes.BOOLEAN,
-      allowNull: false,
-      default: false
-    }
+    chinese: { type: DataTypes.STRING },
+    frequency: { type: DataTypes.INTEGER },
+    banned: { type: DataTypes.BOOLEAN, defaultValue: false }
   }, {
-    timestamps: true,
     classMethods: {
       associate: () => {
         Word.belongsToMany(models.text, { through: 'wordText' });

commit 281f260ed63d95e4affeca722ca91733d455c5c7

Fixes bug introduced in 5d6eb06

src/models/charText.js
@@ -5,8 +5,18 @@ export default (sequelize, DataTypes) => {
     charId: { type: DataTypes.INTEGER },
     textId: { type: DataTypes.INTEGER },
     order: { type: DataTypes.INTEGER },
-    manuallyAdded: { type: DataTypes.BOOLEAN },
-    manuallyDeleted: { type: DataTypes.BOOLEAN }
+    // The following constraints are important
+    // Removing them creates a bug when saving items with an important DB
+    manuallyAdded: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: true
+    },
+    manuallyDeleted: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false
+    }
   }, {
     classMethods: {
       associate: () => {

src/models/wordText.js
@@ -2,11 +2,21 @@ export default (sequelize, DataTypes) => {
   const models = sequelize.models;
   const WordText = sequelize.define('wordText', {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-    order: { type: DataTypes.INTEGER },
     wordId: { type: DataTypes.INTEGER },
     textId: { type: DataTypes.INTEGER },
-    manuallyAdded: { type: DataTypes.BOOLEAN },
-    manuallyDeleted: { type: DataTypes.BOOLEAN }
+    order: { type: DataTypes.INTEGER },
+    // The following constraints are important
+    // Removing them creates a bug when saving items with an important DB
+    manuallyAdded: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: true
+    },
+    manuallyDeleted: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false
+    }
   }, {
     classMethods: {
       associate: () => {