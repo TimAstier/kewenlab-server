9dcfc1589a56d6dfd2793ab2009c5dfa8d5de9be
Tim <timothee.astier@essec.edu>
2017-04-05 15:29:26 +0200

    getProjects route

--- src/services/user-getter.js
+++ src/services/user-getter.js
@@ -2,5 +2,11 @@ import models from '../models';
 
 export default function UserGetter(userId) {
   return models.user
-    .findOne({ where: { id: userId } });
+    .findOne({ where: { id: userId } })
+    .then(user => {
+      if (!user) {
+        throw { status: 404, message: 'not_found' };
+      }
+      return user;
+    });
 }
